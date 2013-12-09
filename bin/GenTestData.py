d5802e666cfe860ab62ad67895cfee2232858595
mouda <dses5211@gmail.com>
2013-12-10 01:03:01 +0800

    problem generation script done

--- bin/GenTestData.py
+++ bin/GenTestData.py
@@ -1,20 +1,57 @@
-#!/usr/bin/python2.6
+#!/usr/bin/python2.7
 import random
+from subprocess import Popen, PIPE
+from subprocess import call
+import re
+import sys
+
+if (len(sys.argv) != 3):
+    print "Usage: [file name] [discard links]"
+    exit()
 
 filePath="../input/P3_friend/"
-fileName="SG1"
+fileName=sys.argv[1]
 fileSuffix=".txt"
-discardLinkNum=1;
+discardLinkNum=int(sys.argv[2]);
+inputFileName=filePath+fileName+fileSuffix
 outputTestName=filePath+fileName+"_test"+fileSuffix
 outputQueryName=filePath+fileName+"_query"+fileSuffix
 
-inputFile = open(filePath+fileName+fileSuffix);
+inputFile = open(inputFileName);
 for line in inputFile:
-    #print line.split()[1], line.split()[1]
     pass
 maxVertex = int(line.split()[0])
 inputFile.close();
-inputFile = open(filePath+fileName+fileSuffix)
-for i in range(discardLinkNum):
-    print random.randint(0,maxVertex)
+linkPairList=[];
+inputFile = open(inputFileName, 'r')
+print outputTestName
+call(["cp",inputFileName, outputTestName])
+testOutputFile = open(outputTestName,'r')
+while len(linkPairList) < discardLinkNum: 
+    nodeIdx = random.randint(0,maxVertex)
+    for line in inputFile:
+        pair = 2*[int]
+        if str(nodeIdx) in line:
+            pair[0] = int(line.split()[0])
+            pair[1] = int(line.split()[1])
+            linkPairList.append(pair)
+            inputFile.seek(0)
+            break
 
+testOutputFile = open(outputTestName,'w')
+queryOutputFile = open(outputQueryName,'w')
+print linkPairList
+for line in inputFile:
+    pair1 = 2*[int]
+    pair2 = 2*[int]
+    pair1[0] = int(line.split()[0])
+    pair1[1] = int(line.split()[1])
+    pair2[0] = int(line.split()[1])
+    pair2[1] = int(line.split()[0])
+    if (pair1 in linkPairList) or (pair2 in linkPairList):
+        queryOutputFile.write(line);
+    else:
+        testOutputFile.write(line);
+
+testOutputFile.close()
+queryOutputFile.close()
