b585a0cc7a77e77054a157bbc094ef34f3c07350
mouda <dses5211@hotmail.com>
2013-12-10 14:24:10 +0800

    add run script

---
+++ bin/run.sh
@@ -0,0 +1,2 @@
+#./linkPrediction ../input/P3_friend/SG1_test_1000.txt SG1_model_1000.model train 
+./linkPrediction ../input/P3_friend/SG1_test_1000.txt SG1_model_1000.model inference:../input/P3_friend/SG1_query_1000.txt
