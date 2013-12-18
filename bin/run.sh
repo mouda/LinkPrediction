ec7a69baa81fdf7d24813b2e515c983cdfac0bfc
mouda <dses5211@hotmail.com>
2013-12-18 20:23:05 +0800

    update

--- bin/run.sh
+++
@@ -1,2 +0,0 @@
-#./linkPrediction ../input/P3_friend/SG1_test_1000.txt SG1_model_1000.model train 
-./linkPrediction ../input/P3_friend/SG1_test_1000.txt SG1_model_1000.model inference:../input/P3_friend/SG1_query_1000.txt
