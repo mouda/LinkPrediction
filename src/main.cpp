532810218ea5565aa45149ad4ade6bbe0dd6e81f
mouda <dses5211@hotmail.com>
2014-01-09 22:58:56 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -2,100 +2,116 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <iterator>
 
 #include "socialSystem.h"
 #include "utility.h"
+#include <boost/program_options.hpp>
 
-
-//#include <boost/mpi.hpp>
-
-//// Enable PBGL interfaces to BGL algorithms
-//#include <boost/graph/use_mpi.hpp>
-//
-//// Communicate via MPI
-//#include <boost/graph/distributed/mpi_process_group.hpp>
-
+namespace po = boost::program_options;
 
 using namespace std;
 
 int main( int argc, char* argv[] )
 {
-
-  if ( argc != 4 ) {
-    cout << "Usage: [input] [model] [train:<train file>|inference:<test file>:<true|false>|printTrain:<train file>|printTest:<file name>:<true|false>]" << endl;
-    return 1;
-  }
-//  boost::mpi::environment env(argc,argv);
-//  boost::mpi::communicator world;
-
   string inputFileName(argv[1]);
   string outputFileName(argv[2]);
   string strFlag(argv[3]);
+  string trainFileName;
+  bool boolPrintFlag=false;
+  bool boolAnswerFlag=false;
+  try {
+    po::options_description desc("Allowed options");
+    desc.add_options()
+      ("help,h", "Produce help message")
+      ("graph,g", po::value<string>(), "Graph file name")
+      ("model,m", po::value<string>(), "Model file name")
+      ("train,t", po::value<string>(), "Train file name")
+      ("inference,i", po::value<string>(), "Tnference file name")
+      ("answer,a", po::value<bool>(), "Answer, true or false")
+      ("print,p", "Use this option to print instance");
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
 
-  if (!FileExist(inputFileName)) {
-    cerr << "Input file doesn't exist" << endl;
+    if (vm.size() == 0 || vm.count("help")) {
+      cout << desc << "\n";
+      return 0;
+    }
+
+    if (vm.count("train") && vm.size() == 3) {
+
+      if (!vm.count("model")) {
+        cerr << "You must provide model file"<<endl;
+      }
+
+      if (!vm.count("graph")) {
+        cerr << "You must provide graph file"<<endl;
+      }
+
+      inputFileName = vm["graph"].as<string>();
+      outputFileName = vm["model"].as<string>();
+      trainFileName = vm["train"].as<string>();
+      if (!FileExist(inputFileName)) {
+        cerr << "Graph file ``" << inputFileName << "'' doesn't exist" << endl;
+        return 1;
+      }
+      if (!FileExist(trainFileName)) {
+        cerr << "Train file ``" << trainFileName << "'' doesn't exist" << endl;
+        return 1;
+      }
+
+      SocialSystem mySocialSystem(inputFileName, outputFileName, 0);
+      mySocialSystem.Train(trainFileName);
+    }
+    else if (vm.count("inference") && (vm.size() == 4 || vm.size() == 5)) {
+
+      if (!vm.count("model")) {
+        cerr << "You must provide model file"<<endl;
+      }
+      if (!vm.count("graph")) {
+        cerr << "You must provide graph file"<<endl;
+      }
+
+      outputFileName = vm["model"].as<string>();
+      inputFileName = vm["graph"].as<string>();
+      string inferenceFileName = vm["inference"].as<string>();
+
+      if (!FileExist(inferenceFileName)) {
+        cerr << "Inference file ``" << inferenceFileName << "'' doesn't exist" << endl;
+        return 1;
+      }
+      if (!FileExist(outputFileName)) {
+        cerr << "Error: model file doesn't exist" << endl;
+        return 1;
+      } 
+      if (!FileExist(inputFileName)) {
+        cerr << "Graph file ``" << inputFileName << "'' doesn't exist" << endl;
+        return 1;
+      }
+
+      boolAnswerFlag =  vm["answer"].as<bool>();
+
+      SocialSystem mySocialSystem(inputFileName, outputFileName, 0);
+      if (!vm.count("print")) {
+        mySocialSystem.ReportCorrectRatio(inferenceFileName, boolAnswerFlag);
+      }
+      else {
+        mySocialSystem.PrintTestInstance(inferenceFileName, boolAnswerFlag);
+      }
+
+    } else {
+      cout << desc << "\n";
+      return 0;
+    }
+
+  }
+  catch(std::exception& e) {
+    cerr << "error: " << e.what() << "\n";
     return 1;
   }
-
-  SocialSystem mySocialSystem(inputFileName, outputFileName, 0);
-  
-  vector<string> flagStrings = split(strFlag,':');
-  if (flagStrings[0] == "train") {
-    if (!FileExist(flagStrings[1])) {
-      cerr << "Error: train file doesn't exist" << endl;
-      return 1;
-    }
-    mySocialSystem.Train(flagStrings[1]);
-  }
-  else if (flagStrings[0] == "inference") {
-    if (!FileExist(outputFileName)) {
-      cerr << "Error: model file doesn't exist" << endl;
-      return 1;
-    } 
-    if (!FileExist(flagStrings[1])) {
-      cerr << "Error: query file doesn't exist" << endl;
-      return 1;
-    } 
-
-    if (flagStrings[2] == "true") {
-      mySocialSystem.ReportCorrectRatio(flagStrings[1], true);
-    }
-    else if (flagStrings[2] == "false") {
-      mySocialSystem.ReportCorrectRatio(flagStrings[1], false);
-    }
-    else {
-      cerr << "Error: unknown predice result" << endl;
-    }
-  } else if (flagStrings[0] == "printTrain"){
-    if (!FileExist(flagStrings[1])) {
-      cerr << "Error: train file doesn't exist" << endl;
-      return 1;
-    }
-    mySocialSystem.PrintTrainInstance(flagStrings[1]);
-  } else if (flagStrings[0] == "printTest") {
-
-    if (!FileExist(outputFileName)) {
-      cerr << "Error: model file doesn't exist" << endl;
-      return 1;
-    } 
-    if (!FileExist(flagStrings[1])) {
-      cerr << "Error: query file doesn't exist" << endl;
-      return 1;
-    } 
-
-    if (flagStrings[2] == "true") {
-      mySocialSystem.PrintTestInstance( flagStrings[1], true);
-    }
-    else if (flagStrings[2] == "false") {
-      mySocialSystem.PrintTestInstance( flagStrings[1], false);
-    }
-    else {
-      cerr << "Error: unknown predice result" << endl;
-    }
-
-  }
-  else {
-    cerr << "Error: unknown option" << endl;
+  catch(...) {
+    cerr << "Exception of unknown type!\n";
   }
   return 0;
 }
